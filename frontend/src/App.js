3a95c42220a1ed21596213cc7aab4f5b86e68524
Đỗ Xuân Thanh <thanhcr2001@gmail.com>
2022-11-27 17:17:05 +0700

    Update Frontend DashBoard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,32 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
+import Footer from './Components/Footer/footer';
+import Header from './Components/Header/header';
+import Main from './Components/Main/main';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Home from './pages/Home'
 import Verify from './pages/Verify'
-
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <Routes>
-          <Route path='/register' element={<Register/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/home' element={<Home/>} />
-          <Route path='/verify' element={<Verify/>} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Router>
+        <div className='App'>
+          <Header />
+          <Main />
+          <Footer />
+          <Routes>
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/verify' element={<Verify />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
+
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyles from './components/GlobalStyles';
@@ -8,8 +10,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyles>
-      <App/>
+      <Router>
+        <App />
+      </Router>
     </GlobalStyles>
+
+
   </React.StrictMode>
 );
 
