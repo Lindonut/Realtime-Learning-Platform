60a3a89e8b9569b38d0cc1bedd54948a7a543a03
Đỗ Xuân Thanh <thanhcr2001@gmail.com>
2022-11-28 08:25:50 +0700

    Update Frontend
    Upgrade front-end affter merrge code

--- frontend/src/components/Main/main.js
+++ frontend/src/components/Main/main.js
@@ -1,24 +1,17 @@
 import React, { useState } from 'react'
 import { Modal, ModalHeader } from 'reactstrap'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, Router, Outlet } from 'react-router-dom'
 import '../../../src/App.css'
 import Dashboard from './dashboard/dashboard'
 import Infomation from './infomation/infomation'
-import Infogroup from './inforgroup/infogroup'
-import Description from './inforgroup/description/description'
-import Member from './inforgroup/member/member'
+import Infogroup from './infogroup/infogroup'
+import Description from './infogroup/description/description'
+import Member from './infogroup/member/member'
 
 const Main = () => {
     return (
         <div className='main'>
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/infomation" element={<Infomation />} />
-                <Route path="/infogroup" element={<Infogroup />}>
-                    <Route path="/infogroup" element={<Description />} />
-                    <Route path="/infogroup/member" element={<Member />} />
-                </Route>
-            </Routes>
+                <Outlet/>
         </div>
     )
 }
